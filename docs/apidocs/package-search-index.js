df2ea7687cf0ac186443731189ee92933967fdc2
Pietro Saccone <pietrosaccone@gmail.com>
2026-04-13 23:49:19 +0200

    java doc update

--- docs/apidocs/package-search-index.js
+++ docs/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.simexid.keycloak"},{"l":"org.simexid.keycloak.enums"},{"l":"org.simexid.keycloak.exception"},{"l":"org.simexid.keycloak.model"},{"l":"org.simexid.keycloak.service"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.simexid.keycloak"},{"l":"org.simexid.keycloak.enums"},{"l":"org.simexid.keycloak.exception"},{"l":"org.simexid.keycloak.model"},{"l":"org.simexid.keycloak.service"}];updateSearchResults();
